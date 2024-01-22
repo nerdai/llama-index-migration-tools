7ff980bdc79934d1f4a3a770e132f305f7634b0d
Andrei Fajardo <andrei@nerdai.io>
2024-01-22 15:16:34 -0500

    make test init file

--- llama_index_migration_tools/main.py
+++ llama_index_migration_tools/main.py
@@ -7,6 +7,9 @@ def _create_init_file(dir: str):
     # create __init__.py
     Path(dir + "/__init__.py").touch() 
 
+def _create_test_file(filename: str):
+    Path(filename).touch()
+
 def _makedirs(dir: str):
     try:
         os.makedirs(dir)
@@ -54,6 +57,10 @@ def main(integration_type: str, integration_name: str):
                 NAME=integration_name.lower().title()
             )
         )
+    
+    # create an empty test file
+    test_file_name = tests_path + f"/test_{integration_type}_{integration_name}.py".replace(" ", "_").lower()
+    _create_test_file(test_file_name)
     
     # copy common files to folders
     script_path = Path(__file__).parent.resolve()
