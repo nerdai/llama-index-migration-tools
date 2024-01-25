609da2245b184a5e11472afb1ce4fec3cc55e976
Andrei Fajardo <andrei@nerdai.io>
2024-01-25 13:00:50 -0500

    add init template

--- llama_index_migration_tools/main.py
+++ llama_index_migration_tools/main.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 from pathlib import Path
-from llama_index_migration_tools.templates import pyproject_str, readme_str
+from llama_index_migration_tools.templates import pyproject_str, readme_str, init_str, init_with_prefix_str
 from typing import Optional
 
 def _create_init_file(dir: str):
@@ -18,15 +18,20 @@ def _makedirs(dir: str):
         pass
 
 
-def main(integration_type: str, integration_name: str, base_file: Optional[str] = None):
+def main(integration_type: str, integration_name: str, prefix: Optional[str] = None, base_file: Optional[str] = None):
     # create new directory, works in current directory
-    pkg_name = f"llama-index-{integration_type}-{integration_name}".replace(" ", "-").replace("_", "-").lower()
+    pkg_name = (
+        f"llama-index-{integration_type}-{integration_name}".replace(" ", "-").replace("_", "-").lower() if prefix is None else 
+        f"llama-index-{prefix}-{integration_type}-{integration_name}".replace(" ", "-").replace("_", "-").lower()           
+    )
     pkg_path = os.path.join(os.getcwd(), pkg_name)
     tests_path = os.path.join(pkg_path, "tests")
     examples_path = os.path.join(pkg_path, "examples")
     pkg_src_dir = os.path.join(
         pkg_path,
-        f"llama_index/{integration_type}/{integration_name}".replace(' ', '_').lower()
+        (f"llama_index/{integration_type}/{integration_name}".replace(' ', '_').lower() if prefix is None else
+        f"llama_index/{prefix}/{integration_type}/{integration_name}".replace(' ', '_').lower() 
+         )
     )
 
     # make dirs
@@ -37,7 +42,16 @@ def main(integration_type: str, integration_name: str, base_file: Optional[str] 
 
     # create init files
     _create_init_file(tests_path)
-    _create_init_file(pkg_src_dir)
+    with open(pkg_src_dir + "/__init__.py", "w") as f:
+        init_string = init_str.format(
+            TYPE=integration_type.replace(" ", "_").lower(),
+            NAME=integration_name.replace(" ", "_").lower(),
+        ) if prefix is None else init_with_prefix_str.format(
+            TYPE=integration_type.replace(" ", "-").lower(),
+            NAME=integration_name.replace(" ", "-").lower(),
+            PREFIX=prefix.replace(" ", "_").lower()
+        )
+        f.write(init_string)
 
     # create pyproject.toml
     with open(pkg_path + "/pyproject.toml", "w") as f:
@@ -60,7 +74,9 @@ def main(integration_type: str, integration_name: str, base_file: Optional[str] 
         )
     
     # create an empty test file
-    test_file_name = tests_path + f"/test_{integration_type}_{integration_name}.py".replace(" ", "_").lower()
+    test_file_name = tests_path + (f"/test_{integration_type}_{integration_name}.py".replace(" ", "_").lower() if prefix is None else 
+            f"/test_{prefix}_{integration_type}_{integration_name}.py".replace(" ", "_").lower()
+        )
     _create_test_file(test_file_name)
     
     # copy common files to folders
--- llama_index_migration_tools/templates/__init__.py
+++ llama_index_migration_tools/templates/__init__.py
@@ -1,4 +1,5 @@
 from llama_index_migration_tools.templates.pyproject import pyproject_str
 from llama_index_migration_tools.templates.readme import readme_str
+from llama_index_migration_tools.templates.init import init_str, init_with_prefix_str
 
-__all__ = ["pyproject_str", "readme_str"]
+__all__ = ["pyproject_str", "readme_str", "init_str", "init_with_prefix_str"]
