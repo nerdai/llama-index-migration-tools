4ec9ad2e18b9bbc3b9746082508401535b4d763e
Andrei Fajardo <andrei@nerdai.io>
2024-02-10 00:59:50 -0500

    script for automatically updating pyproject.toml

--- llama_index_migration_tools/main_update_pyproject.py
+++ llama_index_migration_tools/main_update_pyproject.py
@@ -1,7 +1,35 @@
-import toml
+from typing import Dict
+from .update_pyproject_toml import update_pyproject_toml
+import json
+import os
 
-def main(toml_path: str):
-    pyproject = toml.load(toml_path)
-    pyproject["tool"]["poetry"]["version"] = "0.0.1"
-    with open(toml_path, "w") as f:
-        toml.dump(pyproject, f)
+
+def main():
+    # pyproject.tomls list
+    with open(
+        os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "llamaindex_registry.txt"
+        ),
+    "r") as f:
+        list = f.read().split('\n')
+    
+    # version_registry
+    with open(
+        os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "version_registry.json"
+        ),
+    "r") as f:
+        version_registry = json.load(f)
+
+    for package in list:
+        if not package:
+            continue
+        print(package)
+        toml_path = os.path.join(package, "pyproject.toml")
+        update_pyproject_toml(toml_path=toml_path, version_registry=version_registry)
+
+
+if __name__ == "__main__":
+    main()
---
+++ llama_index_migration_tools/update_pyproject_toml.py
@@ -0,0 +1,19 @@
+from typing import Dict
+import toml
+
+def update_pyproject_toml(toml_path: str, version_registry: Dict[str, str]):
+    pyproject = toml.load(toml_path)
+    poetry_config = pyproject["tool"]["poetry"]
+
+    # update version according to version_registry
+    package_name = poetry_config["name"]
+    pyproject["tool"]["poetry"]["version"] = version_registry[package_name]
+
+    # update llama-index package versions in list of deps
+    dependencies = poetry_config["dependencies"]
+    for dep in dependencies.keys():
+        if dep in version_registry:
+            dependencies[dep] = version_registry[dep]
+
+    with open(toml_path, "w") as f:
+        toml.dump(pyproject, f)
